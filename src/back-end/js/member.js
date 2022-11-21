284584d71d10dfe436582ad8ec94412ea4a851b1
Mandy_Li <c26931230@gmail.com>
2022-11-21 22:01:10 +0800

    新增後台會員頁面

---
+++ src/back-end/js/member.js
@@ -0,0 +1,103 @@
+Vue.createApp({
+    data() {
+        return {
+            member: [ //會員資訊
+                {
+                    id: 1,
+                    name: "Mandy",
+                    subscribe: "Basic",
+                    mail:"c123123@gmail.com",
+                    tel:"0912312312",
+                    style:"極簡",
+                    type: "沙漏",
+                },
+                {
+                    id: 2,
+                    name: "Mandy",
+                    subscribe: "Basic",
+                    mail:"c123123@gmail.com",
+                    tel:"0912312312",
+                    style:"極簡",
+                    type: "沙漏",
+                },
+                {
+                    id: 3,
+                    name: "Mandy",
+                    subscribe: "Basic",
+                    mail:"c123123@gmail.com",
+                    tel:"0912312312",
+                    style:"極簡",
+                    type: "沙漏",
+                },
+                {
+                    id: 4,
+                    name: "Mandy",
+                    subscribe: "Basic",
+                    mail:"c123123@gmail.com",
+                    tel:"0912312312",
+                    style:"極簡",
+                    type: "沙漏",
+                },
+                {
+                    id: 5,
+                    name: "Mandy",
+                    subscribe: "Basic",
+                    mail:"c123123@gmail.com",
+                    tel:"0912312312",
+                    style:"極簡",
+                    type: "沙漏",
+                },
+                {
+                    id: 6,
+                    name: "Mandy",
+                    subscribe: "Basic",
+                    mail:"c123123@gmail.com",
+                    tel:"0912312312",
+                    style:"極簡",
+                    type: "沙漏",
+                },
+                {
+                    id: 7,
+                    name: "Mandy",
+                    subscribe: "Basic",
+                    mail:"c123123@gmail.com",
+                    tel:"0912312312",
+                    style:"極簡",
+                    type: "沙漏",
+                },
+                {
+                    id: 8,
+                    name: "Mandy",
+                    subscribe: "Basic",
+                    mail:"c123123@gmail.com",
+                    tel:"0912312312",
+                    style:"極簡",
+                    type: "沙漏",
+                },
+                {
+                    id: 9,
+                    name: "Mandy",
+                    subscribe: "Basic",
+                    mail:"c123123@gmail.com",
+                    tel:"0912312312",
+                    style:"極簡",
+                    type: "沙漏",
+                },
+                {
+                    id: 10,
+                    name: "Mandy",
+                    subscribe: "Basic",
+                    mail:"c123123@gmail.com",
+                    tel:"0912312312",
+                    style:"極簡",
+                    type: "沙漏",
+                },
+                
+                
+                
+            ],	
+        }
+        },
+        methods:{
+        }
+    }).mount('#app')
