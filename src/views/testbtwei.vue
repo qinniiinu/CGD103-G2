053657fb6674506ab3559a6edac7efbbfdb1c64e
master <114372757+Sid0478@users.noreply.github.com>
2022-11-23 18:01:23 +0800

    test again

---
+++ src/views/testbtwei.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>訂閱頁面</div>
+	<Footer></Footer>
+</template>
+<script>
+import Footer from "@/components/Footer.vue";
+
+export default {
+	name: "About",
+	components: {
+		Footer,
+	},
+};
+</script>
