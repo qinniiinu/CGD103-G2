ae82eed7e9b2088903e287e63a17b02e389db1f9
master <114372757+Sid0478@users.noreply.github.com>
2022-11-23 17:55:54 +0800

    for test

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,15 @@ const routes = [
 		component: () => import("../views/AboutView.vue"),
 	},
 	{
+		path: "/product",
+		name: "product",
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		// component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+		component: () => import("../views/product.vue"),
+	},
+	{
 		path: "/subscription",
 		name: "subscription",
 		component: () => import("../views/Subscription.vue"),
