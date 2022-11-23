568f003cf7566e3765defb446aa4aa39db266ddc
cinnnk@gmail.com <cinnnk@gmail.com>
2022-11-23 22:26:35 +0800

    增加 jquery + bootstrap

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "bootstrap";
+// import "sass/main.scss";
+// import "bootstrap/sass/bootstrap.scss";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
 createApp(App).use(store).use(router).mount("#app");
