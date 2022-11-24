dad6c50dd3e294c0bf3e41f75abdc7f70904ff61
jason_yt <38196149+jason-yt@users.noreply.github.com>
2022-11-24 11:42:17 +0800

    icon int

--- src/main.js
+++ src/main.js
@@ -2,9 +2,24 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "bootstrap";
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret,faCartShopping,faUser } from '@fortawesome/free-solid-svg-icons'
+
+
+/* add icons to the library */
+library.add(faUserSecret,faCartShopping,faUser)
+
+
+// import AccountCircleIcon from 'node_modules/@mui/icons-material/AccountCircle';
+// import "bootstrap";
 // import "sass/main.scss";
 // import "bootstrap/sass/bootstrap.scss";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount("#app");
