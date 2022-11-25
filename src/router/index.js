b104ba0c2f8e54c3e3393712c4ec6b33c0f099cf
jason_yt <38196149+jason-yt@users.noreply.github.com>
2022-11-25 10:28:24 +0800

    改index.js記得要說

--- src/router/index.js
+++ src/router/index.js
@@ -186,11 +186,11 @@ const routes = [
 		name: "Consultating",
 		component: () => import("../views/style/ConsultatingView.vue"),
 	},
-	{
-		path: "/ConsultIntro",
-		name: "ConsultIntro",
-		component: () => import("../views/style/ConsultIntroView.vue"),
-	},
+	// {
+	// 	path: "/testtest",
+	// 	name: "testtest",
+	// 	component: () => import("../views/style/testtestView.vue"),
+	// },
 	{
 		path: "/List",
 		name: "List",
