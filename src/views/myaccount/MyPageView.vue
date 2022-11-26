6eaab5b71510df622e462adc6bd9ff2b76571f40
JiaChun0216 <jasmine204635@gmail.com>
2022-11-26 18:49:38 +0800

    修改 會員主頁共用版面

--- src/views/myaccount/MyPageView.vue
+++ src/views/myaccount/MyPageView.vue
@@ -1,11 +1,15 @@
 <template>
 	<!-- 會員 container -->
 	<div class="mem_container">
-		<!-- 側邊攔 -->
-		<SideMenu></SideMenu>
-		<!-- 右側資料-->
+		<!-- 側邊欄組件 -->
+		<MemberSideMenu></MemberSideMenu>
+		<!-- 右側會員資料-->
 		<div class="mem_data">
-			<h1>右側內容</h1>
+			<!-- 依頁面更改標題 -->
+			<p class="title">Costomer, 歡迎回來</p>
+			<div class="data">
+				<!-- 這裡開始寫 -->
+			</div>
 		</div>
 	</div>
 	<Footer></Footer>
@@ -13,24 +17,16 @@
 
 <script>
 import Footer from "@/components/Footer.vue";
-import SideMenu from "@/components/SideMenu.vue";
-
+import MemberSideMenu from "@/components/MemberSideMenu.vue";
 export default {
 	name: "MyPage",
 	components: {
-		Footer,SideMenu
+        Footer,MemberSideMenu
 	},
 };
 
 </script>
-<style lang="scss">
-@import "@/assets/sass/mulstyle";
-    .mem_container{
-		display: flex;
-		width: 100% ;
-		margin: auto;	
-		.mem_data{
-		flex-grow: 1;
-		}
-	}
+<style lang="scss" scoped>
+@import "@/assets/sass/member_side_menu";
+
 </style>
