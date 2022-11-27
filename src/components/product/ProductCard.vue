0ae35dacd7fb78795696511780c4fb752900bbba
cinnnk@gmail.com <cinnnk@gmail.com>
2022-11-27 10:50:38 +0800

    輪播圖 、商品

---
+++ src/components/product/ProductCard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="product_card">
+        <div class="box">
+            <img src="https://fakeimg.pl/300x200/200" />
+            <font-awesome-icon icon="fa-solid fa-heart" class="icon" />
+        </div>
+        <p class="title">UNISEX 環保刷毛帽T</p>
+        <p class="price">NT$ 1,980</p>
+    </div>
+</template>
+<style lang="scss" scoped>
+.product_card {
+    padding-bottom: 10px;
+    .box {
+        margin: 5px;
+        border: $line solid $title_color;
+        aspect-ratio: 1/1;
+        position: relative;
+        .icon {
+            position: absolute;
+            font-size: 20px;
+            top: 0;
+            right: 0;
+            padding: 5px;
+        }
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: contain;
+        }
+    }
+    p {
+        margin-left: 10px;
+        font-size: 14px;
+        padding: 5px;
+    }
+}
+</style>
