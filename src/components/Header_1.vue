c41854087edd7e7bc4acec16bd9c1fed0cf75f81
jason_yt <38196149+jason-yt@users.noreply.github.com>
2022-11-27 21:47:06 +0800

    Update Header_1.vue

---
+++ src/components/Header_1.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav class="main-nav">
+        <ul>
+            <h1></h1>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+	name: "Header",
+
+	components: {
+
+	},
+	
+  	data() {
+    	return {
+			links:[
+				{herf:'/MulStyles'},
+				{herf:'/Product'},
+				{herf:'/Ootd'},
+				{herf:'/Subscription'},
+				{herf:'/About'},
+			],
+			pages:[
+				'找風格',
+				'逛商品',
+				'風特動態牆',
+				'訂閱服務',
+				'關於我們',
+			],
+			currentTag:'#上班穿搭',
+
+    	} 
+  	},
+	computed:{
+		tagComponent(){
+			return `tag-${this.currentTag.toLowerCase()}`;
+		},
+	},
+
+
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
