abae03f05403d8ac05c8b88075beaf32b95ff9d3
JiaChun0216 <jasmine204635@gmail.com>
2022-11-27 14:03:12 +0800

    各會員頁共用版面

--- src/views/myaccount/OrderDetailsView.vue
+++ src/views/myaccount/OrderDetailsView.vue
@@ -1,16 +1,32 @@
 <template>
-	<div>
-        <h1>OrderDetailsView</h1>
-    </div>
+	<!-- 會員 container -->
+	<div class="mem_container">
+		<!-- 側邊欄組件 -->
+		<MemberSideMenu></MemberSideMenu>
+		<!-- 右側會員資料-->
+		<div class="mem_data">
+			<p class="title">訂單明細</p>
+			<div class="data">
+				<!-- 這裡開始寫 -->
+			</div>
+		</div>
+	</div>
 	<Footer></Footer>
 </template>
+
 <script>
 import Footer from "@/components/Footer.vue";
-
+import MemberSideMenu from "@/components/MemberSideMenu.vue";
 export default {
 	name: "OrderDetails",
 	components: {
-		Footer,
+        Footer,
+		MemberSideMenu
 	},
 };
 </script>
+
+<style lang="scss" scoped>
+@import "@/assets/sass/member_side_menu";
+
+</style>
--- src/views/myaccount/SubscriptionView.vue
+++ src/views/myaccount/SubscriptionView.vue
@@ -1,16 +1,32 @@
 <template>
-	<div>
-        <h1>SubscriptionView</h1>
-    </div>
+	<!-- 會員 container -->
+	<div class="mem_container">
+		<!-- 側邊欄組件 -->
+		<MemberSideMenu></MemberSideMenu>
+		<!-- 右側會員資料-->
+		<div class="mem_data">
+			<p class="title">訂閱服務</p>
+			<div class="data">
+				<!-- 這裡開始寫 -->
+			</div>
+		</div>
+	</div>
 	<Footer></Footer>
 </template>
+
 <script>
 import Footer from "@/components/Footer.vue";
-
+import MemberSideMenu from "@/components/MemberSideMenu.vue";
 export default {
-	name: "subscription",
+	name: "SubscriptionView",
 	components: {
-		Footer,
+        Footer,
+		MemberSideMenu
 	},
 };
 </script>
+
+<style lang="scss" scoped>
+@import "@/assets/sass/member_side_menu";
+
+</style>
