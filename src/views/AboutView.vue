f7dc024d047f957c3b04587b616acf50103edc2e
Wei <114372757+Sid0478@users.noreply.github.com>
2022-11-28 21:15:21 +0800

    main.js 新增icon router 新增         path: "/OrderHistoryPhoto",        path: "/OrderHistoryDetail",兩個頁面 關於我們 新增一張相片 新增  訂單記錄頁面

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,18 @@
 <template>
-	<div class="about">
-		<h1>This is an about page</h1>
+	<div class="landing_page_photo">
+		<img src="../assets/images/about-landing-page.jpg" alt="">
+	
 	</div>
 	<Footer></Footer>
 </template>
 
 <script>
-import Footer from "@/components/Footer.vue";
+
 import Header from "@/components/Header.vue";
 
 export default {
 	name: "About",
 	components: {
-		Footer,
 		Header,
 	},
 };
