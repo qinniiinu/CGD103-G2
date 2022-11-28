6065bcc1768c5dc29e4097040e2021de7b04b4c8
mandy <c26931230@gmail.com>
2022-11-28 13:49:54 +0800

    新增風格測驗結果

---
+++ src/views/style/quiz/QuizResultView.vue
@@ -0,0 +1,26 @@
+<template>
+	<section class="quiz_idx">
+		<!-- <router-link :to="{ name:'QuizResult1' }">QuizResult1</router-link>
+		<router-link :to="{ name:'QuizResult2' }">QuizResult2</router-link> -->
+	<router-view/>
+	</section>
+	<Footer></Footer>
+</template>
+<script>
+import Footer from "@/components/Footer.vue";
+import StyleCard1 from "@/components/StyleCard1.vue";
+import Button from "@/components/Button.vue";
+
+export default {
+	name: "Quiz",
+	components: {
+		Footer,
+		StyleCard1,
+		Button,
+	},
+};
+</script>
+<style lang="scss" scoped>
+
+
+</style>
