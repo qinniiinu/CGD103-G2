74837e0a54794634bd025eff220d49f6b04550ca
JiaChun0216 <jasmine204635@gmail.com>
2022-11-30 17:18:14 +0800

    add components

---
+++ src/components/mypage/BigTag.vue
@@ -0,0 +1,30 @@
+<template>
+    <p class="bg_tag">#{{tagName}}
+    </p>
+</template> 
+
+
+<script>
+export default {
+    props: {
+        tagName: String,
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.bg_tag {
+    position: absolute;
+    z-index: -1;
+    right: 10px;
+    color: $bg_violet;
+    @include font_set($second_font, $null, 900, italic);
+    @include s() {
+        font-size: 56px;
+    }
+    @include m() {
+        font-size: 72px;
+    }
+}
+</style>
