c8224af2d437b9f7e45d576a058e45acaba6ff24
jason_yt <38196149+jason-yt@users.noreply.github.com>
2022-12-01 15:58:40 +0800

    Update ConsultatingView.vue

--- src/views/style/ConsultatingView.vue
+++ src/views/style/ConsultatingView.vue
@@ -4,33 +4,67 @@
 			{{step}}
 		</p>
 	</div>
+	<div class="consultqa">
+		<div class="consultqa-title">
+			<h2></h2>
+			<p></p>
+		</div>
+		<div class="consultqa-btn">
+			<div class="consultqa-btn-item">
+
+			</div>
+		</div>
+		<div class="consultqa-input">
+			<div class="consultqa-input-title">
+				<h3>
+
+				</h3>
+			</div>
+			<textarea class="consultqa-textarea">
+
+			</textarea>
+			<Button></Button>
+		</div>
+	</div>
 
 
 
 </template>
 <script>
-
+import Button from "@/components/Button.vue"
 
 export default {
 	name: "Consultating",
 	components: {
-
+		Button,
 	},
 	data(){
 		return{
-			steps:['STEP1','STEP2','STEP3']
+			steps:['STEP1','→','STEP2','→','STEP3']
 		}
 	}
 };
 </script>
 <style lang="scss" scoped>
 	.step{
+
 		display: flex;
+		justify-content: center;
 		p{
+
 			padding: 20px;
 			border-radius: 50%;
 			background-color:$main_color;
 			color: $second_color;
+			font-weight: 700;
+			font-size: 24px;
+		}
+		p:nth-child(2n){
+			border-radius: 0%;
+			background-color:transparent;
+			color: $main_color;
+			font-size: 20px;
+			font-weight: 400;
 		}
 	}
 </style>
