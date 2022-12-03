14735c4fcb58befd12346807e8a15f7afdd6c3f2
Mandy_Li <c26931230@gmail.com>
2022-12-03 20:38:16 +0800

    風格測驗結果頁面

---
+++ src/views/style/quiz/ResultSportView.vue
@@ -0,0 +1,22 @@
+<template>
+    <QuizResult 
+    result="運動風" 
+    img1="./hipster1.jpg" 
+    img2="./hipster2.png"
+    desc="運動休閒風日愈成為主流趨勢，從以前因運動需求才穿上運動裝，到現在運動裝已經普遍成為了一種常見的穿搭風格，大家對於運動休閒接受度越來越高，不僅不會覺得奇怪，甚至覺得穿上運動裝，就無形的展現著一種「對生活有紀律、有熱情」的生活態度。"
+    scardP="./stylist-2.jpg"
+    stylistName="Mary"
+    stylistInfo="擅長運動風格,用前衛的單品,搭配出衝突的美感"
+     />
+  </template>
+  
+  <script>
+  import QuizResult from "@/components/QuizResult.vue";
+  export default {
+    components: {
+      QuizResult,
+    },
+  
+  
+  }
+  </script>
