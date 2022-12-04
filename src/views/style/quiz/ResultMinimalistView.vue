4be588a3e3942e9b461ce6a93f3f796e8b7306bc
Mandy_Li <c26931230@gmail.com>
2022-12-04 10:24:10 +0800

    更新風格測驗照片

--- src/views/style/quiz/ResultMinimalistView.vue
+++ src/views/style/quiz/ResultMinimalistView.vue
@@ -1,8 +1,8 @@
 <template>
   <QuizResult 
   result="極簡風" 
-  img1="./hipster1.jpg" 
-  img2="./hipster2.png"
+  img1="./minimalist1.png" 
+  img2="./minimalist2.png"
   desc="極簡風在穿搭時，也可以嘗試看看多層次的穿搭法。選擇色系相近、長短不同的衣服，穿搭時可以營造出層次感，讓衣著看上去不會過度呆版。例如選擇長版的素色薄外套搭配條紋的T恤，或是利用襯衫當作外套，內襯一件素色T恤也是相當好的搭配。這樣的穿搭法除了可以增加視覺的豐富度，面對季節交替又或是夏天冷氣房與室外的溫差變化，都能靈活的穿脫，可說是兼具實用性與時尚性的文青風格。"
   scardP="./stylist-2.jpg"
   stylistName="Mary"
