61f041b863eaa1f99a2af87d02f3f84cbaaab020
jason_yt <38196149+jason-yt@users.noreply.github.com>
2022-12-06 14:55:57 +0800

    前後台切換

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-	<div class="app">
+	<div class="login" v-if="(isLogin==false)">
+		<div class="front_end longin_box" @click="(this.isLogin=true)">
+			<p>前台</p>
+		</div>
+		<div class="back_end longin_box" >
+			<p>後台</p>
+		</div>
+	</div>
+	<div class="home_page" v-else>
 		<Header></Header>
 		<router-view/>
 		<Footer></Footer>
@@ -21,16 +29,43 @@ export default {
 	
   	data() {
     	return {
+			isLogin:false,
 
     	} 
   	},
 	computed:{
 	
 	},
-
+	methods:{
+		LoginBackend(){
+			// this.isLogin=true
+			// console.log(this.isLogin)
+		}
+	},
 
 };
 </script>
 <style lang="scss" scoped>
-
+	.login{
+		display: flex;
+		width: 100%;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+	.longin_box{
+		width: 100px;
+		height: 100px;
+		background-color: $main_color;
+		margin: 20px;
+		p{
+			// display: inline-block;
+			margin: auto;
+			color:$third_color;
+			font-weight: 700;
+			line-height: 100px;
+			font-size: 24px;
+			text-align: center;
+		}
+	}
 </style>
