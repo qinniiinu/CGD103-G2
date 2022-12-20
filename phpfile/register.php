a7a32b09455392c0bec149ee1b09349b5428eccf
JiaChun0216 <jasmine204635@gmail.com>
2022-12-20 15:31:49 +0800

    註冊/身形建置 還沒好

--- phpfile/register.php
+++ phpfile/register.php
@@ -15,30 +15,44 @@ $errMsg = "";
 
 
     // $member = $pdo->prepare( $sql ); //先編譯好
-    // $member->bindValue(":mem_mail", $mem_mail); //代入資料
+    // $member->bindValue(":mem_mail", $mem_mail); 
     // $member->bindValue(":mem_pwd", $mem_pwd);
     // $member->execute();//執行之
 
-    if( $member->rowCount() == 0 ){//登入失敗
-        $errMsg .= "用戶帳號或密碼錯誤請重新登入<br>";
-    }else{ //登入成功,
-        //自資料庫中取回資料
-        $memRow = $member->fetch(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    $errMsg .= "錯誤 : ".$e -> getMessage()."<br>";
+    $errMsg .= "行號 : ".$e -> getLine()."<br>";
+}
+?> 
 
-        $_SESSION['user'] = $memRow;
 
-        //送出登入者的資料
-        // $result = ["memNo"=>$_SESSION["memNo"], "memId"=>$_SESSION["memId"],"memName"=>$_SESSION["memName"],"email"=>$_SESSION["email"]];
-        // echo json_encode($result);
+<!-- 前台 身形建置 -->
+<?php
+header('Access-Control-Allow-Origin:*');
+header("Content-Type:application/json;charset=utf-8");
+try {
+$chest = $_POST["chest"];
+$shoulder = $_POST["shoulder"];
+$waistline = $_POST["waistline"];
+$hip = $_POST["hip"];
+$clothes = $_POST["clothes"];
+$pants = $_POST["pants"];
+$hip = $_POST["hip"];
+$shoesize = $_POST["shoesize"];
+$height = $_POST["height"];
+$weight = $_POST["weight"];
+$errMsg = "";
 
-        // 設定回應的 HTTP 標頭
-        header('Content-Type: application/json');
-        // 將會員資料輸出為 JSON 格式
-        echo json_encode($user);
+require_once("../connectBooks.php");
 
-        header('Location: /MyPage'); // 自動跳轉至會員頁
-        exit;
-    }
+$sql = "INSERT INTO `member` (chest, shoulder, waistline, clothes, pants) VALUES ($chest, $shoulder, $waistline, $clothes, $pants, $shoesize, $height,$weight)"; 
+
+
+// $member = $pdo->prepare( $sql ); //先編譯好
+// $member->bindValue(":mem_mail", $mem_mail); 
+// $member->bindValue(":mem_pwd", $mem_pwd);
+// $member->execute();//執行之
+
 } catch (PDOException $e) {
     $errMsg .= "錯誤 : ".$e -> getMessage()."<br>";
     $errMsg .= "行號 : ".$e -> getLine()."<br>";
