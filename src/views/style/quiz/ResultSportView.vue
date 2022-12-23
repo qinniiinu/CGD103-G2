e16e6de8208c82bdfb649e36d475a344f2a50c8d
mandy <c26931230@gmail.com>
2022-12-23 14:15:45 +0800

    修正風格圖路徑

--- src/views/style/quiz/ResultSportView.vue
+++ src/views/style/quiz/ResultSportView.vue
@@ -9,7 +9,7 @@
     <section class="recommend_box">
       <div v-for="e in product" class="item" :key="e.product_pic" >
          <router-link :to="`/productDetails/${e.product_id}`">
-          <img :src="require(`@/assets/product/${cut(e.product_pic)}`)" />
+          <img :src="`/pic/${cut(e.product_pic)}`" />
           <h4>{{ e.product_name }}</h4>
         </router-link>
       </div>
