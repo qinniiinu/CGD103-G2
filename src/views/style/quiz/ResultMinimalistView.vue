d2f64ed7a609a1b891a0f6bad097288d944f5064
mandy <c26931230@gmail.com>
2022-12-23 19:44:16 +0800

    紀錄風格

--- src/views/style/quiz/ResultMinimalistView.vue
+++ src/views/style/quiz/ResultMinimalistView.vue
@@ -5,12 +5,12 @@
     img2="/pic/minimalist2.png"
     desc="極簡風在穿搭時，也可以嘗試看看多層次的穿搭法。選擇色系相近、長短不同的衣服，穿搭時可以營造出層次感，讓衣著看上去不會過度呆版。例如選擇長版的素色薄外套搭配條紋的T恤，或是利用襯衫當作外套，內襯一件素色T恤也是相當好的搭配。這樣的穿搭法除了可以增加視覺的豐富度，面對季節交替又或是夏天冷氣房與室外的溫差變化，都能靈活的穿脫，可說是兼具實用性與時尚性的文青風格。"
   />
-   <button class="btn_ns" @click="insert()">記錄我的風格</button>
+  <button class="btn_ns" @click="insert">記錄我的風格</button>
   <h3 class="recommend">推薦商品</h3>
   <section class="recommend_box">
     <div v-for="e in product" class="item" :key="e.product_pic">
       <router-link :to="`/productDetails/${e.product_id}`">
-       <img :src="`/pic/${cut(e.product_pic)}`" />
+        <img :src="`/pic/${cut(e.product_pic)}`" />
         <h4>{{ e.product_name }}</h4>
       </router-link>
     </div>
@@ -29,7 +29,7 @@ export default {
   data() {
     return {
       product: [],
-      style_id:104,
+      style_id: 104,
     };
   },
   methods: {
@@ -45,16 +45,15 @@ export default {
       if (x) return x.split(",")[0];
     },
     insert() {
-			// console.log(this.qaBtncheck.toString());
-			const payload={
-				mem_style_id:this.style_id,
-			}
-			fetch(`${BASE_URL}/record_style.php`,{
-				method:'post',
-				body:new URLSearchParams(payload)
-			})
-      alert("ok");
-		},
+      // const data = {
+      //   mem_style_id: this.style_id,
+      // } 
+
+      fetch(`${BASE_URL}/record_style.php`, {
+        method: "POST",
+        body: new URLSearchParams(`mem_style_id= 123`),
+      }).then((res)=>res.json()).then((json)=>console.log(json));
+    }
   },
   mounted() {
     this.getResource();
