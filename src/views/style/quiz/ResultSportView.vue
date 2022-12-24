2ed96513e83daced65a8a2296c02477c9f393c04
mandy <c26931230@gmail.com>
2022-12-24 20:56:42 +0800

    簡化推薦商品寫法 修正 Loading

--- src/views/style/quiz/ResultSportView.vue
+++ src/views/style/quiz/ResultSportView.vue
@@ -5,7 +5,7 @@
     img2="./pic/sport2.png"
     desc="運動休閒風日愈成為主流趨勢，從以前因運動需求才穿上運動裝，到現在運動裝已經普遍成為了一種常見的穿搭風格，大家對於運動休閒接受度越來越高，不僅不會覺得奇怪，甚至覺得穿上運動裝，就無形的展現著一種「對生活有紀律、有熱情」的生活態度。"
      />
-       <button class="btn_ns" @click="insert">記錄我的風格</button>
+       <button class="btn_ns" @click="record_style">記錄我的風格</button>
       <h3 class="recommend">推薦商品</h3>
     <section class="recommend_box">
       <div v-for="e in product" class="item" :key="e.product_pic" >
@@ -28,21 +28,39 @@ export default {
    data() {
     return {
       product:[],
+      style_id: 101,
     }
   },
   methods:{
-     getResource() {
-      this.axios.get(`${BASE_URL}/quiz/sport_recommend.php`).then((response) => {
-                this.product = response.data;
-                console.log(this.product);
-            });
-    },
      cut(x) {
       if (x) return x.split(",")[0];
     },
+      record_style() {
+      const data = {
+        style_id: this.style_id,
+      };
+      fetch(`${BASE_URL}/mem_styleUPD.php`, {
+        method: "post",
+        body: new URLSearchParams(data),
+      })
+        .then((res) => res.json())
+        .then((json) => console.log(json));
+        alert("記錄成功");
+    },
+    getRecommend() {
+      const data = {
+        style_id: this.style_id,
+      };
+      fetch(`${BASE_URL}/quiz/prod_recommend.php`, {
+        method: "post",
+        body: new URLSearchParams(data),
+      })
+        .then((res) => res.json())
+        .then((json) => (this.product = json));
+    },
   },
    mounted() {
-    this.getResource();
+    this.getRecommend();
   }
 
 
