716b8174245ffdb5bb4203437e34cfa82d4ca0aa
mandy <c26931230@gmail.com>
2022-12-24 17:20:19 +0800

    風格測驗修正功能

--- src/views/style/quiz/ResultFashionView.vue
+++ src/views/style/quiz/ResultFashionView.vue
@@ -5,6 +5,7 @@
   img2="./pic/fashion3.png"
   desc="時尚風在穿搭時，也可以嘗試看看多層次的穿搭法。選擇色系相近、長短不同的衣服，穿搭時可以營造出層次感，讓衣著看上去不會過度呆版。例如選擇長版的素色薄外套搭配條紋的T恤，或是利用襯衫當作外套，內襯一件素色T恤也是相當好的搭配。這樣的穿搭法除了可以增加視覺的豐富度，面對季節交替又或是夏天冷氣房與室外的溫差變化，都能靈活的穿脫，可說是兼具實用性與時尚性的風格。"
    />
+     <button class="btn_ns" @click="getRecommend">記錄我的風格</button>
     <h3 class="recommend">推薦商品</h3>
    <section class="recommend_box">
     <div v-for="e in product" class="item" :key="e.product_pic">
@@ -27,6 +28,7 @@ export default {
   data() {
     return {
       product: [],
+      // style_id:103,
     };
   },
   methods: {
@@ -41,9 +43,21 @@ export default {
     cut(x) {
       if (x) return x.split(",")[0];
     },
+     getRecommend() {
+      const data = {
+        style_id: 103,
+      };
+      fetch(`${BASE_URL}/mem_styleUPD.php`, {
+        method: "post",
+        body: new URLSearchParams(data),
+      })
+        .then((res) => res.json())
+        .then((json) => console.log(json));
+    },
   },
   mounted() {
     this.getResource();
+    // this.getRecommend();
   },
 };
 </script>
