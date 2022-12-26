f0d4b70018772fdaaa38a7651345ae220d2c27c2
mandy <c26931230@gmail.com>
2022-12-26 13:48:38 +0800

    會員登入可以記錄風格

--- phpfile/mem_styleUPD.php
+++ phpfile/mem_styleUPD.php
@@ -5,14 +5,12 @@ header("Content-Type:application/json;charset=UTF-8");
 $errMsg = "";
     require_once("../connectBooks.php");
     $style_id = (int)$_POST['style_id'];
-    // 抓不到 store*****
-    // $member_id = (int)$_REQUEST['mem_id']; 
-    $member_id = $_SESSION['member']['mem_id']; 
-    // $member_id = 117; 
+    $member_id = (int)$_POST['mem_id']; 
     $sql = "update member set style_id = $style_id where mem_id = $member_id;";
     $products = $pdo->prepare($sql);	
     $products->execute();
- 
+
+// echo json_encode("test");
 ?>
    
 
--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,6 +1,7 @@
 // 上線用
 // export const BASE_URL = 'https://tibamef2e.com/cgd103/g2/front/phpfile'
 // 本地端
-// export const BASE_URL = 'http://localhost/cgd103_g2_frontend/phpfile'
+export const BASE_URL = 'http://localhost/cgd103_g2_frontend/phpfile'
+// export const BASE_URL = 'api_server'
 // MAC用
-export const BASE_URL = 'http://localhost:8887/cgd103_g2_frontend/phpfile'
+// export const BASE_URL = 'http://localhost:8887/cgd103_g2_frontend/phpfile'
